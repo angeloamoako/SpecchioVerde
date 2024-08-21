37b1862b0ba686b0bfd74ccacbdc5c88eb0e8637
angeloamoako <angelomicro@hotmail.com>
2024-08-21 23:42:18 +0200

    Aggiunta requests come dipendenza

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Specchio Verde
 
 Web app della mostra interattiva per Afroveronesi Summer Festival 2024
+
+
+
+Dipendenze:
+
+- requests: installabile da pip tramite il comando
+`$ python -m pip install requests`
--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
+
+    r = requests.post("http://bugs.python.org", data={'number': '12524', 'type': 'issue', 'action': 'show'})
+    print(r.status_code, r.reason)
+
     return 'Hello World!'
 
 
